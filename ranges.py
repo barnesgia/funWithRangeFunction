5fb2e4f4cbfdf051d81b13e5abfcd1b91456d6d2
barnesgia <barnesgia@gmail.com>
2017-01-25 17:16:15 -0500

    uploaded file

---
+++ ranges.py
@@ -0,0 +1,9 @@
+#count upward from 0-3
+"""for i in range(4):
+    print(i)"""
+#count backward from 3-0
+"""for i in range(3,-1,-1):
+    print (i)"""
+#count even numbers backward from 8
+for k in range(8,0,-2):
+    print (k)
